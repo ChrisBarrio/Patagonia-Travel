1b8f682a82206da7e8bac2f41a6cb5c2574587c7
ChrisBarrio <ch.barrionuevo89@gmail.com>
2022-06-19 15:30:56 -0300

    commit 'se agregan divs desde arrays'

--- js/app.js
+++ js/app.js
@@ -211,3 +211,123 @@ var swiper = new Swiper(".mySwiperAdds", {
     
   } 
 });
+
+// data featured products
+
+const products = [
+  {
+    img:'./images/product-1.png',
+    nombre:'survival kit',
+    precio:'usd 20',
+  },
+  {
+    img:'./images/product-2.png',
+    nombre:'navaja mod.3',
+    precio:'usd 12',
+  },
+  {
+    img:'./images/product-3.png',
+    nombre:'gafas snow',
+    precio:'usd 22',
+  },
+  {
+    img:'./images/product-4.png',
+    nombre:'botas de trekking',
+    precio:'usd 120',
+  },
+  {
+    img:'./images/product-5.png',
+    nombre:'gancho de seguridad',
+    precio:'usd 18',
+  },
+  {
+    img:'./images/product-6.png',
+    nombre:'asador portatil',
+    precio:'usd 50',
+  },
+];
+
+const listaProductos = products.map(function(e) {
+  return(
+    `
+    <div class="swiper-slide">
+      <img src=${e.img} alt=${e.nombre}>
+      <h3>${e.nombre}</h3>
+      <p>${e.precio}</p>
+      <div>
+          <i class='bx bxs-star'></i>
+          <i class='bx bxs-star'></i>
+          <i class='bx bxs-star'></i>
+          <i class='bx bxs-star'></i>
+          <i class='bx bxs-star-half'></i>
+      </div>
+    </div>
+    `)
+});
+
+document.querySelector("#slideProduct").innerHTML = listaProductos.join('')
+
+//
+
+// data popular packages
+
+const packages = [
+  {
+    img:'./images/img-1.jpg',
+    titulo:'Kayak en lago esmeralda',
+    descripcion:'Navega por el mitico lago esmeralda',
+    precioDesde:'$150',
+    precioHasta:'$200',
+  },
+  {
+    img:'./images/img-2.jpg',
+    titulo:'Isla Bonita',
+    descripcion:'Visita la bella isla y tomate grandes fotos',
+    precioDesde:'$250',
+    precioHasta:'$300',
+  },
+  {
+    img:'./images/img-3.jpg',
+    titulo:'Trekking por lago esmeralda',
+    descripcion:'Disfruta de los senderos mas famosos de Argentina para hacer trekking',
+    precioDesde:'$100',
+    precioHasta:'$150',
+  },
+  {
+    img:'./images/img-4.jpg',
+    titulo:'El Chalten',
+    descripcion:'Conoce la capitalnacional de trekking',
+    precioDesde:'$350',
+    precioHasta:'$400',
+  },
+  {
+    img:'./images/img-5.jpg',
+    titulo:'Costa piedras',
+    descripcion:'Las mejores olas del atlantico',
+    precioDesde:'$100',
+    precioHasta:'$120',
+  },
+  {
+    img:'./images/img-6.jpg',
+    titulo:'Mountain Bike por las Sierras',
+    descripcion:'Para conocer Cordoba de una manera distinta',
+    precioDesde:'$50',
+    precioHasta:'$100',
+  },
+  
+];
+
+const listaPackages = packages.map(function(e) {
+  return(
+    `
+    <div>
+      <img src=${e.img} alt=${e.titulo}>
+      <h3>${e.titulo}</h3>
+      <p>${e.descripcion}</p>
+      <p>${e.precioDesde} - ${e.precioHasta}</p>
+      <a href="#">Explore Now</a>
+    </div>
+    `)
+});
+
+document.querySelector("#dataPackages").innerHTML = listaPackages.join('')
